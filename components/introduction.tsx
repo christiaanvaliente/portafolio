d2b1ec5d8dc056b5c585ff41ade6f45e1fb144b8
christiaanvaliente <104175418+christiaanvaliente@users.noreply.github.com>
2025-01-03 17:02:52 -0300

    Actualizar introduction.tsx mt 4

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,7 +5,7 @@ import { TypeAnimation } from 'react-type-animation';
 const Introduction = () => {
     return (
         <div className="z-20 w-full bg-darkBg/60">
-            <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
+            <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2 mt-4">
                 <Image src="/avatar marca personal home sf.png" priority width="800" height="800" alt="Avatar" />
                 <div className="flex flex-col justify-center max-w-md">
                     <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Tengo habilidades en, <br />
