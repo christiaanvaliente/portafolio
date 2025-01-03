20fb31ae45c0c8e6d1a39b33cedae005b4b41887
christiaanvaliente <104175418+christiaanvaliente@users.noreply.github.com>
2025-01-03 14:31:29 -0300

    Actualizar page.tsx

--- app/(routes)/testimonials/page.tsx
+++ app/(routes)/testimonials/page.tsx
@@ -18,7 +18,7 @@ const TestimonialsPage = () => {
                 <CircleImage />
                 <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
                     Algunos comentarios
-                    <span className="block font-bold text-secondary"> de nuestros clientes</span>
+                    <span className="block font-bold text-secondary"> de nuestros clientes.</span>
                 </h1>
                 <div className="flex items-center justify-center">
                     <div>
