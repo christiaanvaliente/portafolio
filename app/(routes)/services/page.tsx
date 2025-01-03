56239844deed2d04247fb1514ba174600a0401a7
christiaanvaliente <104175418+christiaanvaliente@users.noreply.github.com>
2025-01-03 17:24:35 -0300

    Actualizar page.tsx sin mt-4

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -11,7 +11,7 @@ const ServicesPage = () => {
             <CircleImage />
             <AvatarServices />
             <div className="grid items-center justify-center h-screen max-w-5xl gap-6 mx-auto 
-            md:grid-cols-2 md:px-20 mt-4">
+            md:grid-cols-2 md:px-20">
                 <div className="max-w-[450px] mt-20 md:mt-0 px-4">
 
                     <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5 mt-8">
