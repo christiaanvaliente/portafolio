75295f7f307961feac6d2813c418a6e3ef471bab
christiaanvaliente <104175418+christiaanvaliente@users.noreply.github.com>
2025-01-03 07:52:09 -0300

    Actualizar page.tsx

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -14,7 +14,7 @@ const ServicesPage = () => {
             md:grid-cols-2 md:px-20">
                 <div className="max-w-[450px] mt-20 md:mt-0">
 
-                    <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5">
+                    <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5 mt-8">
                         Mis {" "}
                         <span className="font-bold text-secondary"> 
                             servicios.
